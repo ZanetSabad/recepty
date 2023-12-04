5f4c44c2b8c8b8d30915f47b96df88d5e640e7cc
Žaneta Sabadka <zaneta.sabadkova@gmail.com>
2023-12-04 12:09:18 +0100

    Založení nových webových stránke Recepty

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recepty</title>
+</head>
+<body>
+    <h1>Recepty</h1>
+
+    <nav>
+        <li><a href="">Snídaně</a></li>
+        <li><a href="">Oběd</a></li>
+        <li><a href="">Večeře</a></li>
+        <li><a href="">Svačina</a></li>
+        <li><a href="">Cukroví</a></li>
+        <li><a href="">Buchty</a></li>
+        <li><a href="">Ostatní</a></li>
+    </nav>
+    
+</body>
+</html>
