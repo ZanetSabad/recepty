b29af43b5d3ccda0f3739a2a0e5107edf269fb4b
Žaneta Sabadka <zaneta.sabadkova@gmail.com>
2023-12-06 14:38:14 +0100

    lehce nastylovaná header.php

--- assets/header.php
+++ assets/header.php
@@ -1,5 +1,6 @@
-<h1> Recepty </h1>
-<nav>
-    <li><a href="breakfast.php">Zpět na seznam receptů</a></li>
-    <li><a href="index.php">Zpět na úvodní stránku</a></li>
-</nav>
+<header>
+    <nav>
+        <li><a href="breakfast.php">Seznam receptů</a></li>
+        <li><a href="index.php">Úvodní stránku</a></li>
+    </nav>
+</header>
