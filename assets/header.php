86ac21517550d0dfed4310f509c55b00d9bd8693
Žaneta Sabadka <zaneta.sabadkova@gmail.com>
2023-12-06 14:29:03 +0100

    přidaná navigace do header.php

--- assets/header.php
+++ assets/header.php
@@ -1,6 +1,5 @@
 <h1> Recepty </h1>
 <nav>
-    <!-- <li><a href=""></a></li>
-    <li><a href=""></a></li>
-    <li><a href=""></a></li> -->
+    <li><a href="breakfast.php">Zpět na seznam receptů</a></li>
+    <li><a href="index.php">Zpět na úvodní stránku</a></li>
 </nav>
